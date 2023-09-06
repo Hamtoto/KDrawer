d061a9c4ebbd0a19075847884c6c6b74b3a1af01
Hamtoto <gohg0901@naver.com>
2023-09-06 19:26:33 +0900

    screenWidth, screenHeight 인라인으로 변경 및 이미지 로드 부분 주석처리

--- kdr/gui/dlg/InfoDialog.java
+++ kdr/gui/dlg/InfoDialog.java
@@ -4,12 +4,9 @@ import javax.swing.*;
 import java.awt.*;
 
 public class InfoDialog extends JDialog {
-    private int frameWidth = 400;
-    private int frameheight = 300;
+    private final int frameWidth = 400;
+    private final int frameheight = 300;
     private InfoPanel infoPanel = new InfoPanel();
-
-    private final int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
-    private final int screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
 
     public void InfoDialogFalseVisible() {
         this.setVisible(false);
@@ -18,12 +15,12 @@ public class InfoDialog extends JDialog {
     public class InfoPanel extends JPanel {
         private JLabel[] labels = new JLabel[4];
         private JButton okBut = new JButton("확인");
-        private ImageIcon logoIcon = new ImageIcon("logo.png");
-        private Image logoImage = logoIcon.getImage();
+        //private ImageIcon logoIcon = new ImageIcon("logo.png");
+        //private Image logoImage = logoIcon.getImage();
 
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
-            g.drawImage(logoImage, frameWidth / 2 - 25, 20, 50, 50, null);
+            //g.drawImage(logoImage, frameWidth / 2 - 25, 20, 50, 50, null);
             g.drawRect(10, 10, 360, 240);
         }
 
@@ -39,7 +36,7 @@ public class InfoDialog extends JDialog {
             labels[2] = new JLabel("Department of Computer Science In");
             labels[2].setBounds(frameWidth / 2 - 105, 150, 210, 30);
             add(labels[2]);
-            labels[3] = new JLabel("Busan University of Foreign Studies ");
+            labels[3] = new JLabel("Busan University of Foreign Studies");
             labels[3].setBounds(frameWidth / 2 - 105, 180, 210, 30);
             add(labels[3]);
 
@@ -51,6 +48,10 @@ public class InfoDialog extends JDialog {
     }
     public InfoDialog(String title) {
         super((JFrame) null, title);
+
+        int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
+        int screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
+
         setModal(true);
         setLocation(screenWidth / 2 - frameWidth / 2, screenHeight / 2 - frameheight / 2);
         setResizable(false);
